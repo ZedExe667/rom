ef0fb0c227a80d1783199328278aceb6a94b706f
ZedExe667 <zahrakarimigtr@gmail.com>
2025-03-06 01:05:54 +0330

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// پیکربندی Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDk7sksreRmnfphQfuM9iF6Az9p76NrUAw",
+    authDomain: "chat-rom-d8e07.firebaseapp.com",
+    databaseURL: "https://chat-rom-d8e07-default-rtdb.firebaseio.com",
+    projectId: "chat-rom-d8e07",
+    storageBucket: "chat-rom-d8e07.firebasestorage.app",
+    messagingSenderId: "488983457088",
+    appId: "1:488983457088:web:60f004500e62efc1e89ea5",
+    measurementId: "G-EYE16CSVN8"
+  };
